176be7f80e631e2e63c71339cd063f77a9099052
Tuzki Zhang <rabbitzhang52@gmail.com>
2016-03-12 16:32:31 +0800

    market opening function

--- common.go
+++ common.go
@@ -4,6 +4,34 @@ import (
 	"time"
 )
 
+// 是否在开盘期间 (周一到周五 9:30-11:30 13:00-15:00)
+func MarketIsOpening() bool {
+	now := time.Now()
+	if now.Weekday() == time.Saturday || now.Weekday() == time.Sunday {
+		return false
+	}
+	if now.Hour() < 9 {
+		return false
+	}
+	if now.Hour() == 9 && now.Minute() < 30 {
+		return false
+	}
+	if now.Hour() == 11 && now.Minute() >= 30 {
+		return false
+	}
+	if now.Hour() == 12 {
+		return false
+	}
+	if now.Hour() == 15 && now.Minute() >= 1 {
+		return false
+	}
+	if now.Hour() > 15 {
+		return false
+	}
+	return true
+}
+
+// @deprecated
 func WaitUntilLater(timeString string) {
 	for {
 		// check time
@@ -16,6 +44,7 @@ func WaitUntilLater(timeString string) {
 	}
 }
 
+// @deprecated
 func WaitUntilEalier(timeString string) {
 	for {
 		// check time
