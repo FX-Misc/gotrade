2447154ebd32d6724c6c87af0f0e37c66b14a280
Tuzki Zhang <rabbitzhang52@gmail.com>
2016-03-16 16:24:41 +0800

    improve MarketIsOpening

--- common.go
+++ common.go
@@ -4,55 +4,37 @@ import (
 	"time"
 )
 
-// 是否在开盘期间 (周一到周五 9:30-11:30 13:00-15:00)
-func MarketIsOpening() bool {
+// 默认判断是否在开盘期间，支持自定义时间段 ([]string{"09:30:00", "11:30:00"}, []string{"13:00:00", "15:00:00"})
+// 时间参数必须严格到 xx:xx:xx 否则会返回 false
+func MarketIsOpening(durations ...[]string) bool {
 	now := time.Now()
 	if now.Weekday() == time.Saturday || now.Weekday() == time.Sunday {
 		return false
 	}
-	if now.Hour() < 9 {
+	nowStr := now.Format("15:04:05")
+	if len(durations) == 0 {
+		if (nowStr >= "09:30:00" && nowStr <= "11:30:00") || (nowStr >= "13:00:00" && nowStr <= "15:00:00") {
+			return true
+		}
 		return false
 	}
-	if now.Hour() == 9 && now.Minute() < 30 {
-		return false
-	}
-	if now.Hour() == 11 && now.Minute() >= 30 {
-		return false
-	}
-	if now.Hour() == 12 {
-		return false
-	}
-	if now.Hour() == 15 && now.Minute() >= 1 {
-		return false
-	}
-	if now.Hour() > 15 {
-		return false
-	}
-	return true
-}
-
-// @deprecated
-func WaitUntilLater(timeString string) {
-	for {
-		// check time
-		nowString := time.Now().Format("15:04:05")
-		if nowString <= timeString {
-			time.Sleep(1 * time.Second)
-		} else {
-			break
+	for _, duration := range durations {
+		if len(duration) != 2 {
+			continue
+		}
+		var start, end time.Time
+		var err error
+		start, err = time.Parse("15:04:05", duration[0])
+		if err != nil {
+			continue
+		}
+		end, err = time.Parse("15:04:05", duration[1])
+		if err != nil {
+			continue
+		}
+		if nowStr >= start.Format("15:04:05") && nowStr <= end.Format("15:04:05") {
+			return true
 		}
 	}
-}
-
-// @deprecated
-func WaitUntilEalier(timeString string) {
-	for {
-		// check time
-		nowString := time.Now().Format("15:04:05")
-		if nowString >= timeString {
-			time.Sleep(1 * time.Second)
-		} else {
-			break
-		}
-	}
+	return false
 }
