9543f615f66173933082f0f0434edcbee55840cf
aimsam <1thblood@gmail.com>
2016-04-16 17:32:20 +0800

    reduce sleep time

--- subscriber.go
+++ subscriber.go
@@ -193,7 +193,7 @@ func (sbr *Subscriber) Run() {
 			flag += 1
 			go api.Run()
 			// 分散 worker 防止并发量过大
-			time.Sleep(time.Second * 1)
+			time.Sleep(time.Millisecond * 500)
 			if end >= length {
 				break
 			}
